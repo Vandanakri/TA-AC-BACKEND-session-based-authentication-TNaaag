2b76885d70c6cd9b08cd777cfb435e52b1c6e504
vandana <vandana123annusah@gmail.com>
2022-06-23 17:05:39 +0530

    BNaadw

--- block-BNaadw/Blogapp/routes/index.js
+++ block-BNaadw/Blogapp/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var Blog = require('../models/Blog');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  Blog.find({}, (err, blog) => {
+    if (err) return next(err);
+    res.render('index', { blog: blog });
+  });
 });
 
 module.exports = router;
